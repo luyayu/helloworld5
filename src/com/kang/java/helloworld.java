b0b2b7d1379478cf685a23fc6565222d2e7c7f8e
luyayu <917180726@qq.com>
2021-04-15 19:51:12 +0800

    Initial commit

---
+++ src/com/kang/java/helloworld.java
@@ -0,0 +1,46 @@
+package com.kang.java;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class helloworld {
+    public static void main(String[] args) {//psvm快捷键
+        System.out.println();//sout
+        System.out.println("hello");//"hello".sout
+        System.out.println("你好");
+        System.out.println("helloworld.main");//soutm
+        System.out.println("args = " + Arrays.deepToString(args));//soutp
+        System.out.println("args = " + args);//soutv
+        for (int i = 0; i < 0 ; i++) {//fori
+
+        }
+        for (String arg : args) {//iter
+
+        }
+        for (int i = 0; i < args.length; i++) {//itar
+            String arg = args[i];
+
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<String> list1 = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {//list.fori
+
+        }
+        for (Integer integer : list) {//list.for
+
+        }
+        for (int i = list.size() - 1; i >= 0; i--) {//list.forr
+
+        }
+        if (list == null) {//ifn回车选择参数||list.null
+
+        }
+        if (list1 == null) {
+
+        }
+        if (list != null) {//inn：if not null||list1.nn
+
+        }
+    }
+}
